d9b6f699540781d5679a2a5f15d1de150195e997
kalavhan <josue.brigido1@gmail.com>
2020-02-10 12:53:22 -0700

    Complete style for Homepage view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :AuthorId, presence: true
+  validates :Title, presence: true, length { maximum: 50 }
+  validates :Text, presence: true, length { maximum: 600 }
+  validates :Image, presence: true
 end
