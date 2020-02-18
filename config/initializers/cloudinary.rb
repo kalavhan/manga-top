116aaa225520064b51cfd255d8e0662393b186c9
kalavhan <josue.brigido1@gmail.com>
2020-02-17 22:37:40 -0700

    Modify initializer for cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -4,4 +4,6 @@ Cloudinary.config do |config|
   config.cloud_name = ENV['CLOUDINARY_NAME']
   config.api_key = ENV['CLOUDINARY_API_KEY']
   config.api_secret = ENV['CLOUDINARY_API_SECRET']
+  config.enhance_image_tag= true
+  config.static_file_support= false
 end
