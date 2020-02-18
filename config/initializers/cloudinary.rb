57792790d5dae4acdd7999ddc40fe1dce5c6292c
kalavhan <josue.brigido1@gmail.com>
2020-02-17 23:19:53 -0700

    Update api secret cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -3,7 +3,7 @@
 Cloudinary.config do |config|
   config.cloud_name = ENV['CLOUD_NAME']
   config.api_key = '567197668174973'
-  config.api_secret = ENV['CLOUD_SECRET']
+  config.api_secret = 'dxRCmJQ18xkzkK4m1J7HrYfQHgY'
   config.enhance_image_tag= false
   config.static_file_support= false
 end
