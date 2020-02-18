4a16f33e7119033c4fbbc231898249a46cd936b3
kalavhan <josue.brigido1@gmail.com>
2020-02-17 23:22:28 -0700

    update api name cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Cloudinary.config do |config|
-  config.cloud_name = ENV['CLOUD_NAME']
+  config.cloud_name = 'kalavhan'
   config.api_key = '567197668174973'
   config.api_secret = 'dxRCmJQ18xkzkK4m1J7HrYfQHgY'
   config.enhance_image_tag= false
