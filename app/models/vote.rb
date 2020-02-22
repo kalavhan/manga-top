1155ce27e0373bc015ac6f45115fffae54ea8feb
kalavhan <josue.brigido1@gmail.com>
2020-02-22 00:06:26 -0700

    Verify if signed in to comment

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
-  belongs_to :article, class_name: 'Article'
-  belongs_to :user, class_name: 'User'
+  belongs_to :article
+  belongs_to :user
 
   def self.is_voted?(user_id, article_id)
     Vote.where("user_id = ? AND article_id = ?", user_id, article_id)
