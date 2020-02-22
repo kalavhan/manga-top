01c785b0e772bc2b231e453f0d69d9716895a303
kalavhan <josue.brigido1@gmail.com>
2020-02-21 19:35:35 -0700

    Create model and cotroller for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_200016) do
+ActiveRecord::Schema.define(version: 2020_02_22_023441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2020_02_13_200016) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "priority"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
